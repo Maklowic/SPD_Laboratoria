16d20ca2445a10d082a721808a2afba0ca10bc7a
Maklowic <48453998+Maklowic@users.noreply.github.com>
2021-05-06 17:36:33 +0200

    Add files via upload

--- Laby 8-9/Cw4_Prog_Dynamic.py
+++ Laby 8-9/Cw4_Prog_Dynamic.py
@@ -45,50 +45,6 @@ def Calc_Fmin(p, pi, d, w):
     return F
 
 #
-# procedura Brute Force
-#
-def BruteForce(zad, _N, p, pi, d, w):
-    #global iteracje
-    #iteracje = iteracje+1
-    #print(iteracje)
-    #print("zad: ", zad)
-    bf_N = []
-    bf_N = _N[:]
-    bf_pi = []
-    bf_pi = pi[:]
-    global F_min
-    global pi_prim2
-        
-    bf_pi.append(zad)
-    bf_N.remove(zad)
-
-    for k in bf_N:
-        BruteForce(k, bf_N, p, bf_pi, d, w)
-    #print("pi ", bf_pi)
-    
-    if len(bf_pi) == len(p):
-        F_inne = Calc_Fmin(p, bf_pi, d, w)
-        if F_inne < F_min:
-            F_min = F_inne
-            pi_prim2 = bf_pi[:]
-
-#
-# procedura Greedy
-#
-def Greedy(d):
-    n = len(d)
-    pi = []
-    tmp = d[:]
-    tmp.sort()
-    while tmp:
-        for k in range(0, n):
-            if tmp[0] == d[k]:
-                pi.append(k+1)
-                tmp.remove(d[k])
-                if len(tmp) == 0:
-                    return pi
-
-#
 # czyszczenie bitów
 #
 def bit_clear(val, bit):
@@ -183,55 +139,18 @@ def main():
 
     global T_glob
     global C_glob
+    global wT_glob
     # koniec generowania instancji
     #
     #
 
-    
-    start_time = time.time()
-    
-    pi_greedy = Greedy(d)
-
-    tmp_time = time.time() - start_time
-    F = Calc_Fmin(p, pi_greedy, d, w)
-    print("!!!! po algorytmie Greedy")
-    print("pi: ", pi_greedy)
-    print("C: ", C_glob)
-    print("T: ", T_glob)
-    print("wT: ", wT_glob)
-    print("wiTi = F: ", F)
-    print("Czas działania: %.5s s" % tmp_time)
-
     start_time2 = time.time()
-    for zad in N:
-        bf_N = N[:]
-        BruteForce(zad, bf_N, p, pi, d, w)
-    
+    PD(J, p, d, w, pi)
     tmp_time2 = time.time() - start_time2
 
-    #pi_z_zajec = [2, 5, 1, 7, 4, 8, 3, 6]
-    #pi_z_zajec = [2, 5, 7, 3, 8, 6, 4, 1]
-    #pi_z_zajec = [1, 2, 3, 4, 5, 6, 7, 8]
-
-    F = Calc_Fmin(p, pi_prim2, d, w)
-    print("!!!! po algorytmie Brute Force")
-    print("pi: ", pi_prim2)
-    print("C: ", C_glob)
-    print("T: ", T_glob)
-    print("wT: ", wT_glob)
-    print("F: ", F)
-    
-    print("Czas działania: %.5s s" % tmp_time2)
-
-    pi_n=[]
-
-    start_time2 = time.time()
-    PD(J, p, d, w, pi_n)
-    tmp_time2 = time.time() - start_time2
-
-    F = Calc_Fmin(p, pi_n, d, w)
+    F = Calc_Fmin(p, pi, d, w)
     print("!!!! po algorytmie PD")
-    print("pi: ", pi_n)
+    print("pi: ", pi)
     print("C: ", C_glob)
     print("T: ", T_glob)
     print("wT: ", wT_glob)
--- Laby 8-9/README.md
+++ Laby 8-9/README.md
@@ -1,14 +1,16 @@
-# Laboratorium 4:
+# Labolatorium 4:
 # Grupa 
 * Michał Maćkowiak
 * Michał Dołharz
 # Prowadzący
-* mgr inż. Radosław Idzikowski
+        mgr inż. Radosław Idzikowski
 ## Pliki: 
 ### Cw 4 - problem 1||sum(wiTi)
         - Cw4_Brute_Force.py - implementacja algorytmu przeglądu zupełnego dla problemu 1||sum(wiTi)
 
         - Cw4_Greedy.py - implementacja algorytmu zachłannego dla problemu 1||sum(wiTi)
+
+        - Cw4_Prog_Dynamic.py - implementacja algorytmu programowania dynamincznego inkremetującego dla problemu 1||sum(wiTi)
 
         - Cw4_all_the_code.py - wszystkie napisane algorytmy dla problemu 1||sum(wiTi) w jednym pliku
 
